b58b32596f73c2dd0a72c766b4eb8ca12dbe0615
zobairarib <zoraibariuatasch@gmail.com>
2026-08-21 15:41:02 +0600

    Add files via upload

---
+++ Reverse and Pwn/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+gcc pwnventure.c -o pwnventure \
+    -O0 \
+    -fno-stack-protector \
+    -fPIE \
+    -pie \
+    -Wno-deprecated-declarations \
+    -Wno-implicit-function-declaration \
+    -D_GNU_SOURCE \
+    -Wl,--undefined=gets
+
+echo "[+] Built pwnventure"
